ddceb6af6a39eacc057b440ced395bc5e856aa3f
gavinrufus <61418686+gavinrufus@users.noreply.github.com>
2021-05-31 17:22:25 +0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '822c384546e5fd939d275ff902897578'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6cc71a24-5823-4cfa-ae6d-7f2da3a0043e'
+};
